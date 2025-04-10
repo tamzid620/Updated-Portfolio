29874e7a603b2a02f3c6d79daa42d388e4801cd9
tamzid620 <tamzid620@gmail.com>
2025-04-10 21:48:56 +0600

    app created with basic structure and fonts

---
+++ src/app/(home-layout)/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div>
+      
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/app/config/fonts.js
@@ -0,0 +1,17 @@
+import {  Roboto_Slab, Barlow  } from "next/font/google";
+
+
+
+export const robotoSlab = Roboto_Slab ({
+    weight: [ "100", "200", "300", "400","500", "600", "700", "800", "900"], 
+    subsets: ["latin"],
+    preload: true, 
+});
+
+
+export const barlow = Barlow ({
+    weight: [ "100", "200", "300", "400","500", "600", "700", "800", "900"], 
+     subsets: ["latin"],
+      preload: true, 
+ });
+
