06764181655881f83bbda1aa026d96e5cd479c76
tamzid620 <tamzid620@gmail.com>
2025-05-10 14:34:23 +0600

    sidebar px changed

--- src/app/Components/UserShared/Sidebar/Sidebar.jsx
+++ src/app/Components/UserShared/Sidebar/Sidebar.jsx
@@ -50,7 +50,7 @@ const Sidebar = () => {
   const [hovered, setHovered] = useState(null);
 
   return (
-    <div className="bg-white text-black lg:w-[550px] md:w-[400px] sm: w-full sm: px-4 h-screen pt-10">
+    <div className="bg-white text-black lg:w-[550px] md:w-[400px] sm: w-full sm: px-2 h-screen pt-10">
       {/* image section    */}
       <div className="flex justify-center">
         <div className="bg-purple-900 w-[250px]  p-2  rounded-full shadow-2xl shadow-black">
